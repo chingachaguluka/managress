5364bef24cad60c3dbd5a123e98166f9f4d67073
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2014-02-01 23:15:06 +0200

    Initial Commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Managress::Application.config.secret_key_base = '706d54eca9cd40db2ba8d80a0024d4cde9ddb4c690b8418873bf1d4dab396625079da6554db47c4eb0864131a3e4567dee45f4394b50671413dad9b3e8f62e59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Managress::Application.config.session_store :cookie_store, key: '_managress_session'
